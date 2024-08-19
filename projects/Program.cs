6529a0dba615c0d9ae65b4ab6043d48ce8467ba5
CallieLaw28 <calliebear38@gmail.com>
2024-08-18 23:28:23 -0400

    Add files via upload

---
+++ projects/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+class Movie
+{
+    static void Main()
+    {
+       
+        DisplayMovie("Mama Mia");
+    }
+
+    public static void DisplayMovie(string movie, int minutes = 90)
+    {
+        WriteLine("The movie {Mama Mia} has a running time of {109} minutes.");
+    }
+}
